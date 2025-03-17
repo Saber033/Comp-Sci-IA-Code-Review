104d62986a23183b98f3715e846c36eafac0f386
pete-kugler <pete.kugler@gmail.com>
2025-03-17 00:30:02 -0700

    Just made some trivial formatting changes

--- render_in_place_gui.cpp
+++ render_in_place_gui.cpp
@@ -5,7 +5,7 @@ render_in_place_gui::render_in_place_gui()
 }
 
 ADD_AUDIOPLUGIN_CLASS_TO_CONTAINER(
-	render_in_place,			// Name of the plug-in container for this shared library
-	render_in_place_gui,   // Authoring plug-in class to add to the plug-in container
-	render_in_place_fx		   // Corresponding Sound Engine plug-in class
+	render_in_place,        // Name of the plug-in container for this shared library
+	render_in_place_gui,    // Authoring plug-in class to add to the plug-in container
+	render_in_place_fx      // Corresponding Sound Engine plug-in class
 );
