a0ad51a4abdd627804caa174a27a290f5e00beb6
pete-kugler <pete.kugler@gmail.com>
2025-03-17 00:38:10 -0700

    Added feedback on render_in_place_utilities.h

--- render_in_place_utilities.h
+++ render_in_place_utilities.h
@@ -1,7 +1,10 @@
 // Takes a value in decibels and returns it in linear units
+// $$from-petekug$$: I would mention that it is pre-fixed with "__declspec(dllexport)" so that it can be accessed from your unit tests
 __declspec(dllexport) float db_to_linear_units(float db_value);
 
+// $$from-petekug$$: missing description
 void deserialize_samples(const short* buffer, int sample_count, int buffer_stride, float* out_samples);
 
 // Takes an audio sample buffer stored in floating point format and convert to a 2 byte output buffer format
+// $$from-petekug$$: I would mention that it is pre-fixed with "__declspec(dllexport)" so that it can be accessed from your unit tests
 __declspec(dllexport) void serialize_samples(const float* samples, int sample_count, int buffer_stride, short* out_buffer);
