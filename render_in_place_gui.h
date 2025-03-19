ed394fb2e2eb2a0ebdfba0e247d1b3f8594e49ed
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 20:33:01 -0700

    Add files via upload

--- render_in_place_gui.h
+++ render_in_place_gui.h
@@ -1,6 +1,6 @@
 #include "render_in_place_plugin.h"
 
-//
+//loads the controls to locate the render in place xml and creates controls for it in Wwise
 class render_in_place_gui final
 	: public AK::Wwise::Plugin::GUIWindows
 {
