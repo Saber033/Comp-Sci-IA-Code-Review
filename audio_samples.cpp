1d5e51517730366be1dac6218197ba5db5d2d5a3
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 22:53:02 -0700

    Add files via upload

--- audio_samples.cpp
+++ audio_samples.cpp
@@ -130,23 +130,26 @@ bool audio_samples::load_samples(const char *file_path)
 						for (unsigned int i = 0; i < m_channel_count; i++)
 						{
 							//copies memory from m_samples to another samples buffer to hold past data when multiple files are loaded
-							//copies the memory for each channel
+							//copies the memory for each channel by multiplying the current sample count by the channel index
 							memcpy(new_samples_working_buffer, m_samples + current_sample_count * i, current_sample_count * sizeof(float));
 
-							//working buffer is incremented for the next memcpy for the next channel
+							//working buffer is incremented by the sample size of the old data to prepare for copying the next part of the data
 							new_samples_working_buffer += current_sample_count;
 
-							//converts the binary data in the buffer to a float to be stored in the working buffer
+							//converts the binary data in the buffer to a float which is then stored in the working buffer
 							deserialize_samples(buffer + i, sample_count, m_channel_count, new_samples_working_buffer);
+
+							//working buffer is incremented by the size of data it copied in to make room for the next channel
 							new_samples_working_buffer += sample_count;
 						}
-						//deletes m_samples to stop memory leak and to because the old samples data is no longer needed
+
+						//deletes m_samples to preventing memory leak and to because the old samples data is no longer needed
 						delete[] m_samples;
 
-						//sets the samples member variable to the new samples 
+						//sets the samples member variable to the new samples which contains all of the data that has been combined 
 						m_samples = new_samples;
 
-						//deleting the buffer to get rid of old data and to stop memory leak
+						//deleting the buffer to get rid of old data and to prevent memory leaks
 						delete[] buffer;
 
 						//if everything is correctly read then the function is successful
@@ -155,9 +158,11 @@ bool audio_samples::load_samples(const char *file_path)
 				}
 			}
 		}
+
 		//closes the file
 		fclose(read_ptr);
 	}
+
 	//returns if the loading was successful or not
 	return success;
 }
@@ -198,8 +203,11 @@ bool audio_samples::load_samples(const short* stream, unsigned int sample_count,
 	//1 is the PCM format code
 	m_audio_format = 1;
 
-	//creates a current sample count in case multiple files are being loaded
+	//saving the size of the past data when multiple files are loaded
+	//this will be used to copy data from previous buffers to the new buffer
 	unsigned int current_sample_count = m_sample_count;
+
+	//increasing the size to account for the combined data
 	m_sample_count += sample_count;
 
 	//represents the size of everything in the file 
@@ -207,39 +215,48 @@ bool audio_samples::load_samples(const short* stream, unsigned int sample_count,
 
 	//calculates a total chunk size that represents the size of everything in the file except for the header files
 	//if it's 0 then it's set to the riff header data size, but if not that it adds itself to the data chunk size
-	//if it's 0 it is the first time loading and has not yet combined multiple wav buffers
+	//also note if it's 0 then not combining multiple wav files yet
 	m_total_chunk_size = m_total_chunk_size == 0 ? current_total_chunk_size : m_total_chunk_size + sample_count * m_channel_count * sizeof(short);
 
-	//creates a samples array to hold the data extracted from the wav memory buffer from Wwise
+	//creates a new object that stores the samples in an array of floats
+	//size is determined by how many samples need to be stored multiplied by the channel count to account for multiple audio channels
+	//stored as floats since that is the type used by Wwise for mixing
 	float* new_samples = new float[m_sample_count * m_channel_count];
+
+	//creating a pointer to serve as an index into new_samples without incrementing new_samples*
 	float* new_samples_working_buffer = new_samples;
 
-	//loops through the channels
+	//loops through all audio channels
 	for (unsigned int i = 0; i < m_channel_count; i++)
 	{
 		//copies memory from m_samples to another samples buffer to hold past data when multiple files are loaded
-		//copies the memory for each channel
+		//copies the memory for each channel by multiplying the current sample count by the channel index
 		memcpy(new_samples_working_buffer, m_samples + current_sample_count * i, current_sample_count * sizeof(float));
+
+		//working buffer is incremented by the sample size of the old data to prepare for copying the next part of the data
 		new_samples_working_buffer += current_sample_count;
 
-		//converts the data from the Wwise stream into a float value that can be stored into the buffer
+		//converts the data from the Wwise stream into a float value that is stored into the working buffer
 		deserialize_samples(stream + i, sample_count, m_channel_count, new_samples_working_buffer);
+
+		//working buffer is incremented by the size of data it copied in to make room for the next channel
 		new_samples_working_buffer += sample_count;
 	}
 
 	//deletes the samples to avoid a memory leak and to get rid of the old data
 	delete[] m_samples;
 
-	//setting data to a temporary variable to store for later when multiple files are being loaded
+	//sets the samples member variable to the new samples which contains all of the data that has been combined 
 	m_samples = new_samples;
 
 	//everything ran correctly so it's successful
 	success = true;
 
-	//returns if it's successful or not
+	//returns if the function is successful
 	return success;
 }
 
+//writes the deserialized format into an output wave file
 bool audio_samples::write_samples(const char *file_path)
 {
 	//wb writes into files in binary mode as wav files store binary data
@@ -251,24 +268,32 @@ bool audio_samples::write_samples(const char *file_path)
 	if (write_ptr != nullptr)
 	{
 		wav_riff_header riff_header;
+
 		//sets the riff header data size to the chunk size of everything in the file
 		riff_header.data_size = m_total_chunk_size;
+
 		//wav file id
 		strncpy(riff_header.id, "RIFF", 4);
+
 		//wav file format
 		strncpy(riff_header.format, "WAVE", 4);
+
 		//writes the riff header to the new file
 		fwrite(&riff_header, sizeof(wav_riff_header), 1, write_ptr);
 
 		chunk_header chunks;
+
 		//chunk header id
 		strncpy(chunks.chunk_id, "fmt ", 4);
+
 		//chunk header size
 		chunks.chunk_size = sizeof(wav_fmt_header);
+
 		//writes the data from the chunk header into the new file
 		fwrite(&chunks, sizeof(chunk_header), 1, write_ptr);
 
 		wav_fmt_header fmt_header;
+
 		//initializes all member variables under the format header
 		fmt_header.audio_format = m_audio_format;
 		fmt_header.bits_per_sample = m_bits_per_sample;
@@ -276,25 +301,30 @@ bool audio_samples::write_samples(const char *file_path)
 		fmt_header.byte_rate = m_byte_rate;
 		fmt_header.num_channels = m_channel_count;
 		fmt_header.sample_rate = m_sample_rate;
+
 		//writes the wav fmt header to the new file
 		fwrite(&fmt_header, sizeof(wav_fmt_header), 1, write_ptr);
 
 		chunk_header data_chunk;
+
 		//chunk id
 		strncpy(data_chunk.chunk_id, "data", 4);
-		//calculates the data chunk size using algebraic manipulation
+
+		//calculates the data chunk size which is in bytes and the samples get stored out to each channel
 		data_chunk.chunk_size = m_sample_count * ((m_bits_per_sample / 8) * m_channel_count);
+
 		//writes the chunk header to the new file
 		fwrite(&data_chunk, sizeof(chunk_header), 1, write_ptr);
 
 		//initializes memory for a buffer array that will write to the file
-		//divided by 2 as shorts are half the size of floats/ints
+		//divided by 2 as shorts are double the size of bytes
 		short* buffer = new short[data_chunk.chunk_size / 2];
 
 		//loops through the channels
 		for (unsigned int i = 0; i < m_channel_count; i++)
 		{
 			//converts the samples from a float to binary data which can be written into a wav file
+			//floats are used by Wwise for mixing while shorts are used for the PCM wav file format
 			serialize_samples(m_samples, m_sample_count, m_channel_count, buffer + i);
 		}
 
@@ -306,6 +336,7 @@ bool audio_samples::write_samples(const char *file_path)
 
 		//file is closed
 		fclose(write_ptr);
+
 		//successfully written the file
 		success = true;
 	}
@@ -351,8 +382,9 @@ unsigned int audio_samples::get_channel_count() const
 	return m_channel_count;
 }
 
-//returns the sample count divided by the sample rate divided by the channel count 
-//equals samples/second
+//returns how long the playback of the wav data is in seconds
+//calculated by the sample count divided by the sample rate divided by the channel count 
+//equals (samples/second)/channel
 unsigned int audio_samples::get_duration_seconds() const
 {
 	unsigned int duration = 0;
