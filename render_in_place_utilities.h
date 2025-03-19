2add73e154c020fdc70f9d90b828cd3079c69485
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 20:12:04 -0700

    Add files via upload

--- render_in_place_utilities.h
+++ render_in_place_utilities.h
@@ -1,10 +1,10 @@
-// Takes a value in decibels and returns it in linear units
-//prefixed by __declspec(dllexport) so that it can be accessed from the unit tests
+//takes a value in decibels and returns it in linear units
+//contains __declspec(dllexport) so that it can be accessed from the entry point file for testing
 __declspec(dllexport) float db_to_linear_units(float db_value);
 
 // Takes a Wwise buffer stored in short format and converts it to a 4 byte output buffer format of type float
 void deserialize_samples(const short* buffer, int sample_count, int buffer_stride, float* out_samples);
 
-// Takes an audio sample buffer stored in floating point format and convert to a 2 byte output buffer format
-//prefixed by __declspec(dllexport) so that it can be accessed from the unit tests
+//takes an audio sample buffer stored in floating point format and converts it to a 2 byte output buffer format
+//contains __declspec(dllexport) so that it can be accessed from the entry point file for testing
 __declspec(dllexport) void serialize_samples(const float* samples, int sample_count, int buffer_stride, short* out_buffer);
