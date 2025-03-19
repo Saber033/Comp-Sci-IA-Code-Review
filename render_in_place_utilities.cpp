256a2b6180b5b1bb95ff933fcdc0c489bc0bfd09
pete-kugler <pete.kugler@gmail.com>
2025-03-18 19:15:22 -0700

    formatting cleanup for render_in_place_utilities.cpp

--- render_in_place_utilities.cpp
+++ render_in_place_utilities.cpp
@@ -2,46 +2,47 @@
 #include <math.h>
 #include <iostream>
 
-//takes in a value in db_units and performs a conversion to convert to linear units
+// Takes in a value in db_units and performs a conversion to convert to linear units
 float db_to_linear_units(float db_value)
 {
 	float result = 0.0f;
 
-	//faster to multiply floats than divide
+	// Faster to multiply floats than divide
 	result = powf(10.0f, db_value * 0.05f);
 
 	return result;
 }
 
-//short to float conversion
-//used to take the format from the wav file and convert into the format used by Wwise for digital signal processing - i.e. for volume adjustment, reverberation, etc.
+// Short to float conversion
+// Used to take the format from the wav file and convert into the format used by Wwise for digital signal processing - i.e. for volume adjustment, reverberation, etc.
 void deserialize_samples(const short* buffer, int sample_count, int buffer_stride, float* out_samples)
 {
-	//loops through the sample count
+	// Loops through the sample count
 	for (int i = 0; i < sample_count; i++)
 	{
 		short sample = *buffer;
-		//cast to a float and normalize to the range Wwise expects it in, which is between -1 to 1
+		
+		// Cast to a float and normalize to the range Wwise expects it in, which is between -1 to 1
 		*out_samples = (float)(sample) / SHRT_MAX;
 
-		//increment the location in the output buffer that will be written to next
+		// Increment the location in the output buffer that will be written to next
 		out_samples++;
 
-		//jumping over a stride in the input buffer to make sure only capturing data for the sample channel
+		// Jumping over a stride in the input buffer to make sure only capturing data for the sample channel
 		buffer += buffer_stride;
 	}
 }
 
-//float to short conversion 
-//used to take the format used by Wwise to do the mixing and converts it oteh format that is used for a wav file
+// Float to short conversion 
+// Used to take the format used by Wwise to do the mixing and converts it to the format that is used for a wav file
 void serialize_samples(const float* samples, int sample_count, int buffer_stride, short* out_buffer)
 {
-	//loop through the samples
+	// Loop through the samples
 	for (int i = 0; i < sample_count; i++)
 	{
 		float sample = *samples;
 
-		//preventing overflow
+		// Preventing overflow
 		if (sample > 1.0f)
 		{
 			sample = 1.0f;
@@ -51,13 +52,13 @@ void serialize_samples(const float* samples, int sample_count, int buffer_stride
 			sample = -1.0f;
 		}
 
-		//conversion to a short and denormalizes it from the range Wwise expects, -1 to 1, to something symmetrical with the min/max range for a short
+		// Conversion to a short and denormalized from the range Wwise expects, -1 to 1, to something symmetrical with the min/max range for a short
 		*out_buffer = (short)((float)(SHRT_MAX)*sample);
 
-		//stepping through memory to the next location of the sample
+		// Stepping through memory to the next location of the sample
 		samples++;
 
-		//jump by a stride, writing the current sample from Wwise to the corresponding channel in the buffer we will write to the wav file
+		// Jump by a stride, writing the current sample from Wwise to the corresponding channel in the buffer we will write to the wav file
 		out_buffer += buffer_stride;
 	}
 }
