90bf9e2143f05f2fe1cbaf0de874a9ae3b6595de
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 20:28:21 -0700

    Add files via upload

--- render_in_place_plugin.h
+++ render_in_place_plugin.h
@@ -17,4 +17,4 @@ public:
 	bool GetBankParameters(const GUID& platform_guid, AK::Wwise::Plugin::DataWriter& data_writer) const override;
 };
 
-DECLARE_AUDIOPLUGIN_CONTAINER(render_in_place); //exposes our PluginContainer structure that contains the info for our plugin
+DECLARE_AUDIOPLUGIN_CONTAINER(render_in_place); //exposes our PluginContainer structure so that Wwise can read the info for our plugin
