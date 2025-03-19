8f1c737bbc37eafb5e9dcf9f109e9a987af62827
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 21:00:24 -0700

    Add files via upload

--- render_in_place_fx.h
+++ render_in_place_fx.h
@@ -15,27 +15,27 @@ public:
 	//destructor
 	~render_in_place_fx();
 
-	//plug-in initialization.
-	//prepares the plug-in for data processing, allocates memory and sets up the initial conditions.
+	//plug-in initialization
+	//prepares the plug-in for data processing, allocates memory and sets up the initial conditions
 	AKRESULT Init(AK::IAkPluginMemAlloc* allocator, AK::IAkEffectPluginContext* context, AK::IAkPluginParam* params, AkAudioFormat& format) override;
 
 	//release the resources upon termination of the plug-in.
 	AKRESULT Term(AK::IAkPluginMemAlloc* allocator) override;
 
 	//the reset action should perform any actions required to reinitialize the
-	//state of the plug-in to its original state (e.g. after Init() or on effect bypass).
+	//state of the plug-in to its original state (e.g. after Init() or on effect bypass)
 	AKRESULT Reset() override;
 
 	//plug-in information query mechanism used when the sound engine requires
-	//information about the plug-in to determine its behavior.
+	//information about the plug-in to determine its behavior
 	AKRESULT GetPluginInfo(AkPluginInfo& out_plugin_info) override;
 
 	//effect plug-in DSP execution.
 	void Execute(AkAudioBuffer* buffer) override;
 
-	//skips execution of some frames, when the voice is virtual playing from elapsed time.
-	//this can be used to simulate processing that would have taken place (e.g. update internal state).
-	//return AK_DataReady or AK_NoMoreData, depending if there would be audio output or not at that point.
+	//skips execution of some frames, when the voice is virtual playing from elapsed time
+	//this can be used to simulate processing that would have taken place (e.g. update internal state)
+	//return AK_DataReady or AK_NoMoreData, depending if there would be audio output or not at that point
 	AKRESULT TimeSkip(AkUInt32 frames) override;
 
 private:
