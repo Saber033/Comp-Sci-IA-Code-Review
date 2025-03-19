c4cd25e87bd02b1259b55e2cc812436eb0cc5af3
Saber033 <124924687+Saber033@users.noreply.github.com>
2025-03-18 21:56:10 -0700

    Add files via upload

--- audio_samples.h
+++ audio_samples.h
@@ -2,7 +2,6 @@
 
 //this class creates a wav parser that is based on the wav file format documented on the page below
 //https://www.mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/WAVE.html
-
 //contains __declspec(dllexport) so it can be accessed from entry point for testing
 //the audio samples class contains information from loaded wav data from Wwise or a file, allows for mulitple wav files or data to be loaded and will combine them into a single wav file when write samples is called
 class __declspec(dllexport) audio_samples
